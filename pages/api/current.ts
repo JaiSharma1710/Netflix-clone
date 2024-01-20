075d42480a2758058356f1f7afa14991186d1174
jai sharma <sharmajai901@gmail.com>
2024-01-20 23:45:44 +0530

    done with login and logout feature

---
+++ pages/api/current.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res
+      .status(405)
+      .json({ status: "failed", message: "invalid methord" });
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req);
+    return res.status(200).json({ status: "success", currentUser });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", message: "something went wrong" });
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,36 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
 export default function Home() {
+  const { data } = useCurrentUser();
+  const user = data?.currentUser;
+
   return (
-    <h1>netfilx clone</h1>
-  )
+    <>
+      <h1 className="text-green-500 text-3xl">netfilx clone</h1>
+      <p className="text-white">login as {user?.name}</p>
+      <p className="text-white">login as {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Log Out
+      </button>
+    </>
+  );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
