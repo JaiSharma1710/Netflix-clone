d4ddc8ffd7ef634a295b303ab7e981c51f8005bd
JaiSharma <sharmajai901@gmail.com>
2024-01-22 18:24:36 +0530

    done with movie card

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
+import prismadb from "@/lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res
+      .status(405)
+      .json({ status: "failed", message: "invalid methord" });
+  }
+
+  try {
+    await serverAuth(req);
+    const movies = await prismadb.movie.findMany();
+    return res.status(200).json({ status: "success", data: movies });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", message: "something went wrong" });
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,19 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovies from "@/hooks/useMovies";
 
 const Home = () => {
+  const { data: movies = [] } = useMovies();
+
   return (
     <div>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies.data} />
+      </div>
     </div>
   );
 };
