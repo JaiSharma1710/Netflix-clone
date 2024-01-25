a3570fda58e36f282a2425115d8a827c8a1f6416
JaiSharma <sharmajai901@gmail.com>
2024-01-25 17:24:25 +0530

    added favorite apis

---
+++ pages/api/user-favorites.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prismadb from '@/lib/prismadb';
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+        return res
+        .status(405)
+        .json({ status: "failed", message: "not an allowed http methord" });
+    }
+
+    const { currentUser } = await serverAuth(req, res);
+
+    const favoritedMovies = await prismadb.movie.findMany({
+      where: {
+        id: {
+          in: currentUser?.favoriteIds,
+        }
+      }
+    });
+
+    return res.status(200).json(favoritedMovies);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", message: "something went wrong" });
+  }
+}
